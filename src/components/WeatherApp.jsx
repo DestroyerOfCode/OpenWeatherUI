5049f132b20af278dc8430118d7d767773427b23
DestroyerOfCode <marius.babkovic9@gmail.com>
2021-10-16 15:41:23 +0200

    [FEATURE] improved looks of buttons and added translations

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import WeatherCurrentComponent from './current/WeatherCurrent';
 import { withTranslation } from 'react-i18next';
 import WeatherForecastComponent from './forecast/daily/WeatherForecastDaily';
-// import { temperatureDropdownList } from "../buildingBlocks/commonBuildingBlocks";
 import { TemperatureDropdownList } from '../buildingBlocks/Temperature';
 import TemperatureCtx from '../buildingBlocks/Temperature';
 import i18n from 'i18next';
@@ -13,7 +12,6 @@ import '../i18n';
 import Register from './auth/Register';
 import Login from './auth/Login';
 import Profile from './auth/Profile';
-import { Link } from 'react-router-dom';
 import { logout } from "../actions/auth.actions";
 import { useDispatch } from 'react-redux';
 
@@ -24,85 +22,111 @@ function WeatherApp() {
     });
 
     const dispatch = useDispatch();
+    const history = useHistory();
+
     const logOut = () => {
         dispatch(logout());
       };
 
+    const handlePath = (path) => history.push(path);
+    
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    const temperatureDropdown = () => (
+        <TemperatureDropdownList
+            changeTemperatureUnitsState={(units, abbreviation) => {
+                setTemperature({
+                    units: units,
+                    abbreviation: abbreviation,
+                });
+            }}
+        />                          
+    );
+
     return (
         <main className="container">
-            <Button
-                variant="outlined"
-                onClick={() => i18n.changeLanguage('en')}
-                size="small"
-                disabled={i18n.language === 'en'}
-                color={i18n.language === 'en' ? 'default' : 'primary'}
-                value="en"
-            >
-                EN
-            </Button>
-            <Button
-                variant="outlined"
-                onClick={() => i18n.changeLanguage('sk')}
-                size="small"
-                disabled={i18n.language === 'sk'}
-                color={i18n.language === 'sk' ? 'default' : 'primary'}
-                value="sk"
-            >
-                SK
-            </Button>
-            <Button
-                variant="outlined"
-                onClick={() => i18n.changeLanguage('de')}
-                size="small"
-                disabled={i18n.language === 'de'}
-                color={i18n.language === 'de' ? 'default' : 'primary'}
-                value="de"
-            >
-                DE
-            </Button>
-            <TemperatureDropdownList
-                changeTemperatureUnitsState={(units, abbreviation) => {
-                    setTemperature({
-                        units: units,
-                        abbreviation: abbreviation,
-                    });
-                }}
-            />       
-                <Link
-                    to={{
-                            pathname: "/register"
-                        }}
-                        >
-                            Register
-                </Link>
-                <Link
-                     to={{
-                             pathname: "/login"
-                         }}
-                         >
-                             Log in
-                 </Link>
-                 <Link
-                    onClick={logOut}
-                        >
-                            Log out
-                </Link>
+            <div className = "d-flex justify-content-start">
+                <div className="d-flex justify-content-start">
+                    <Button 
+                        variant="outlined"
+                        size="1g"
+                        onClick={() => handlePath("/")}>
+                        {i18n.t('auth.home')}
+                    </Button>     
+                    <Button 
+                        variant="outlined"
+                        size="1g"
+                        onClick={() => handlePath("/register")}>
+                        {i18n.t('auth.signUp')}
+                    </Button>     
+                    <Button 
+                        variant="outlined"
+                        size="1g"
+                        onClick={() => handlePath("/profile")}>
+                        {user ? i18n.t('auth.profile') : i18n.t('auth.signIn')}
+                    </Button>     
+                    <Button 
+                        variant="outlined"
+                        size="1g"
+                        onClick={logOut}>
+                        {i18n.t('auth.logOut')}
+                    </Button>
+                </div>
+                <div className="d-flex ml-auto p-2 col-example">
+                    <Button
+                        variant="outlined"
+                        onClick={() => i18n.changeLanguage('en')}
+                        size="small"
+                        disabled={i18n.language === 'en'}
+                        color={i18n.language === 'en' ? 'default' : 'primary'}
+                        value="en"
+                    >
+                        EN
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => i18n.changeLanguage('sk')}
+                        size="small"
+                        disabled={i18n.language === 'sk'}
+                        color={i18n.language === 'sk' ? 'default' : 'primary'}
+                        value="sk"
+                    >
+                        SK
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => i18n.changeLanguage('de')}
+                        size="small"
+                        disabled={i18n.language === 'de'}
+                        color={i18n.language === 'de' ? 'default' : 'primary'}
+                        value="de"
+                    >
+                        DE
+                    </Button>
+                </div>
+            </div>
             <Switch>
                 <Route
                     exact
                     path="/"
                     render={(props) => (
-                        <TemperatureCtx.Provider value={temperature}>
-                            <WeatherCurrentComponent {...props} />
-                        </TemperatureCtx.Provider>
+                        <>
+                            {temperatureDropdown()}
+                            <TemperatureCtx.Provider value={temperature}>
+                                <WeatherCurrentComponent {...props} />
+                            </TemperatureCtx.Provider>
+                        </>
                     )}
                 />
                 <Route
                     path="/forecast"
                     render={(props) => (
-                        <TemperatureCtx.Provider value={temperature}>
-                            <WeatherForecastComponent {...props} />
-                        </TemperatureCtx.Provider>
+                        <>
+                            {temperatureDropdown()}
+                            <TemperatureCtx.Provider value={temperature}>
+                                <WeatherForecastComponent {...props} />
+                            </TemperatureCtx.Provider>
+                        </>
                     )}
                 />
                 <Route
