1b64cb709cf25f20db5a57acef3edb622890db7a
DestroyerOfCode <marius.babkovic9@gmail.com>
2021-11-02 15:11:47 +0100

    removed unnecessary console.log

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ import WeatherApp from './components/WeatherApp';
 function App() {
     const dotenv = require('dotenv');
     dotenv.config();
+<<<<<<< HEAD
 
     console.log(process.env);
     return <WeatherApp />;
+=======
+    
+    return (
+        <WeatherApp />
+    )
+>>>>>>> removed unnecessary console.log
 }
 
 export default App;
