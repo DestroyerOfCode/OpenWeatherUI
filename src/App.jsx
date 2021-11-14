2a25da2635cf6f401ef392f9de5ac80283ba3c8e
DestroyerOfCode <marius.babkovic9@gmail.com>
2021-11-14 12:45:45 +0100

    syntax conflict

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import WeatherApp from './components/WeatherApp';
 function App() {
     const dotenv = require('dotenv');
     dotenv.config();
-<<<<<<< HEAD
 
-    console.log(process.env);
     return <WeatherApp />;
-=======
-    
-    return (
-        <WeatherApp />
-    )
->>>>>>> removed unnecessary console.log
 }
 
 export default App;
